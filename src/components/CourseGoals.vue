e35a04dab70479d665084b39bd140fcb2f3cee1e
Justin Llewellyn <jllew24@gmail.com>
2022-06-12 17:46:08 -0700

    init commit

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+	<ul>
+		<li v-for="goal in goals" :key="goal">
+			<slot :goals="goal"></slot>
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				goals: ['Finish course', 'Learn Vue']
+			}
+		}
+	};
+</script>
